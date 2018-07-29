46250ee3410c7093f9d1aa94a9077da1894c4520
Geordon Worley <vadixidav@gmail.com>
2018-07-29 01:32:32 -0400

    refactor out parsing in tests

--- lang/tests/add.rs
+++ lang/tests/add.rs
@@ -5,12 +5,15 @@ extern crate typeflow_lang as tfl;
 use combine::Parser;
 use tf::{env, f};
 
+fn parse(s: &str) -> tf::Expression {
+    tfl::exp()
+        .easy_parse(s)
+        .unwrap_or_else(|e| panic!("{}", e))
+        .0
+}
+
 #[test]
 fn add() {
-    let expression = tfl::exp()
-        .easy_parse("+(@0(2), @1(3))")
-        .unwrap_or_else(|e| panic!("{}", e))
-        .0;
-    let env = env().run(expression);
+    let env = env().run(parse("+(@0(2), @1(3))"));
     assert_eq!(env.implicit("f"), Some(f(5.0)));
 }
